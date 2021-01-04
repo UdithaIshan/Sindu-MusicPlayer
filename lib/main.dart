c8217b40424968df5581c2f7e58f901a040f7b2d
UdithaIshan <uditha.ishan34@gmail.com>
2021-01-04 12:04:30 +0530

    audio player plugin added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_audio_desktop/flutter_audio_desktop.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Taste of Music'),
     );
   }
 }
@@ -29,11 +30,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  AudioPlayer filePlayer;
   int _selectedIndex = 0;
+  GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  String _textFieldValue = '';
+
   @override
+  void initState() {
+    super.initState();
+    this.filePlayer = new AudioPlayer(id: 0, debug: true);
+  }
+
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        key: this._scaffoldKey,
         appBar: AppBar(
           title: Text(widget.title),
         ),
@@ -71,9 +82,116 @@ class _MyHomePageState extends State<MyHomePage> {
                     ],
                   ),
                   Expanded(
-                      child: Container(
-                    color: Colors.black,
-                  ))
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: 150,
+                          ),
+                          SizedBox(
+                            width: 320,
+                            child: TextField(
+                              onChanged: (String value) {
+                                this._textFieldValue = value;
+                              },
+                              decoration: InputDecoration(
+                                hintText: 'Enter path to an audio file...',
+                                labelText: 'Audio Location',
+                                border: OutlineInputBorder(),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 24,
+                          ),
+                          RaisedButton(
+                            child: Container(
+                              child: Text(
+                                "Load File",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            color: Colors.blue,
+                            onPressed: () async {
+                              /// Loading an audio file into the player.
+                              bool result = await filePlayer.load(this._textFieldValue);
+                              if (result) {
+                                this._scaffoldKey.currentState.showSnackBar(SnackBar(
+                                    content: Text(
+                                        'Audio file is loaded. Press FAB to play.')));
+                              } else {
+                                this._scaffoldKey.currentState.showSnackBar(SnackBar(
+                                    content: Text('Audio file is could not be loaded.')));
+                              }
+                            },
+                          ),
+                          IconButton(
+                            icon: filePlayer.isPlaying
+                                ? Icon(Icons.pause)
+                                : Icon(Icons.play_arrow),
+                            iconSize: 36,
+                            color: Colors.blue,
+                            onPressed: () {
+                              this.setState(() {});
+                              if (!filePlayer.isLoaded) {
+                                this._scaffoldKey.currentState.showSnackBar(SnackBar(
+                                  content: Text('Load file first.'),
+                                ));
+                              } else {
+                                if (filePlayer.isPaused) {
+                                  /// Playing loaded audio file.
+                                  this.filePlayer.play();
+                                } else if (filePlayer.isPlaying) {
+                                  /// Pausing playback of loaded audio file.
+                                  this.filePlayer.pause();
+                                }
+                              }
+                            },
+                          ),
+                          Padding(
+                              padding: EdgeInsets.all(5),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.volume_down,
+                                    size: 36,
+                                    color: Colors.blue,
+                                  ),
+                                  Slider(
+                                      divisions: 10,
+                                      value: filePlayer.volume,
+                                      onChanged: (value) {
+                                        this.setState(() {
+                                          /// Changing player volume.
+                                          this.filePlayer.setVolume(value);
+                                        });
+                                      }),
+                                  Icon(
+                                    Icons.device_hub,
+                                    size: 36,
+                                    color: Colors.blue,
+                                  ),
+                                  Slider(
+                                      divisions: filePlayer.devices.length > 0
+                                          ? filePlayer.devices.length
+                                          : 1,
+                                      value: filePlayer.deviceIndex.toDouble(),
+                                      min: 0,
+                                      max: filePlayer.devices.length.toDouble(),
+                                      onChanged: (value) {
+                                        this.setState(() {
+                                          /// Changing player volume.
+                                          this
+                                              .filePlayer
+                                              .setDevice(deviceIndex: value.toInt());
+                                        });
+                                      }),
+                                ],
+                              )),
+                        ],
+                      ),
+                  ),
                 ],
               ),
             ),

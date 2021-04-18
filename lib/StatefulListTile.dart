f2c263793d3906319fad0b0b5fd234e965dc03c8
UdithaIshan <uditha.ishan34@gmail.com>
2021-04-19 01:46:01 +0530

    song title added

--- lib/StatefulListTile.dart
+++ lib/StatefulListTile.dart
@@ -1,22 +1,37 @@
+import 'dart:convert';
+
 import 'package:dart_vlc/dart_vlc.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 class StatefulListTile extends StatefulWidget {
-  const StatefulListTile(
-      {this.title, this.player, this.favs, this.medias, this.index, this.subtitle = ''});
-  final String subtitle, title;
+  StatefulListTile(
+      {this.title, this.player, this.favs, this.medias, this.index, this.isFavs});
+  final String title;
   final List<Media> medias;
   final List<Media> favs;
   final int index;
   final Player player;
+  bool isFavs;
 
   @override
   _StatefulListTileState createState() => _StatefulListTileState();
 }
 
 class _StatefulListTileState extends State<StatefulListTile> {
-  Color _iconColor = Colors.white;
   IconData _icon = Icons.favorite_outline;
+
+  Future<String> getNameOfThis(Media media) async {
+    try {
+      Media metaMedia = await Media.file(new File(media.resource), parse: true);
+      var jsonString = JsonEncoder.withIndent('    ').convert(metaMedia?.metas);
+      var meta = json.decode(jsonString);
+      return meta != null ? meta['title'] : '';
+    } catch (e) {
+      return "Can't get the data 💔";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Container(
@@ -24,7 +39,7 @@ class _StatefulListTileState extends State<StatefulListTile> {
         border: new Border.all(width: 1.0, color: Colors.grey),
       ),
       child: new ListTile(
-        leading: new IconButton(
+        trailing: new IconButton(
           icon: Icon(_icon),
           onPressed: () {
             if (widget?.favs.contains(widget?.medias[widget?.index])) {
@@ -44,23 +59,22 @@ class _StatefulListTileState extends State<StatefulListTile> {
             }
           },
         ),
-        title: new Text(
-          widget?.title ?? "",
-          style: new TextStyle(
-              fontWeight: FontWeight.bold, color: Colors.lightGreen),
-          textAlign: TextAlign.right,
-        ),
-        trailing: new IconButton(
-          icon: Icon(Icons.play_circle_fill_rounded),
-          onPressed: () {
-            print(widget.medias);
-            // widget.player.jump(widget.index);
-            widget.player.open(widget.medias[widget.index], autoStart: true);
-          },
-        ),
-        // subtitle: new Text(widget?.subtitle ?? "",
-        //     textAlign: TextAlign.right, style: TextStyle(color: Colors.white)),
-        // isThreeLine: true,
+        title: FutureBuilder(
+            future: getNameOfThis(widget.medias[widget.index]),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data);
+              }
+              return Text('snapshot.data');
+            }),
+        // trailing: new IconButton(
+        //   icon: Icon(Icons.play_circle_fill_rounded),
+        //   onPressed: () {
+        //     print('in listTile ${widget.index}');
+        //     widget.isFavs = false;
+        //     widget.player.open(widget.medias[widget.index], autoStart: true);
+        //   },
+        // ),
       ),
     );
   }

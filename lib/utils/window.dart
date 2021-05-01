ef4fe47ca310d802dde1b9ada3ae58d67b1a9187
UdithaIshan <uditha.ishan34@gmail.com>
2021-05-01 21:28:05 +0530

    window button colors changed to Color(0xffeb1555)

--- lib/utils/window.dart
+++ lib/utils/window.dart
@@ -4,76 +4,55 @@ import 'package:play_me/models/playerData.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-const sidebarColor = Color(0xff38616d);
-
 class LeftSide extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        width: 200,
+        width: 79.0,
         child: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [backgroundStartColor, backgroundEndColor],
-                  stops: [0.0, 1.0]),
-            ),
+          color: Colors.grey.shade800,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                WindowTitleBarBox(child: Center(child: Text('PlayMe Music Player', ))),
+                WindowTitleBarBox(child: Center(child: Text(''))),    // In here we can add a Window title
               ],
-            )));
+            )
+        )
+    );
   }
 }
-
-const backgroundStartColor = Color(0xFF6c939f);
-const backgroundEndColor = Color(0xFF00296f);
-// const backgroundStartColor = Colors.grey;
-// const backgroundEndColor = Colors.white;
 
 class RightSide extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
         child: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [backgroundStartColor, backgroundEndColor],
-                  stops: [0.0, 1.0]),
-            ),
+            color: Color(0xff303030),
             child: Column(children: [
               WindowTitleBarBox(
                   child: Row(children: [
                 Expanded(child: MoveWindow()),
                 WindowButtons()
               ])),
-            ])));
+            ])
+        )
+    );
   }
 }
-
-final buttonColors = WindowButtonColors(
-    iconNormal: Color(0xFF805306),
-    mouseOver: Color(0xFFF6A00C),
-    mouseDown: Color(0xFF805306),
-    iconMouseOver: Color(0xFF805306),
-    iconMouseDown: Color(0xFFFFD500));
 
 final closeButtonColors = WindowButtonColors(
     mouseOver: Color(0xFFD32F2F),
     mouseDown: Color(0xFFB71C1C),
-    iconNormal: Color(0xFF805306),
-    iconMouseOver: Colors.white);
+    iconNormal: Color(0xffeb1555),
+    iconMouseOver: Colors.white
+);
 
 class WindowButtons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        MinimizeWindowButton(colors: buttonColors),
+        MinimizeWindowButton(colors: closeButtonColors),
         CloseWindowButton(
           colors: closeButtonColors,
           onPressed: () async {
